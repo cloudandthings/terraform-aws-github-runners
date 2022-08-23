ea3b0a1418096234d0c07169cd9a62ec3baa2d25
Bjorn Olsen <bjorn@cloudandthings.io>
2022-08-23 09:58:33 +0000

    f

--- software.tf
+++ software.tf
@@ -1,5 +1,5 @@
 locals {
-  packages_python3 = ["python3", "python-is-python3"]
+  packages_python3 = ["python3", "python-is-python3", "python3-pip"]
 
   packages_docker_engine = ["ca-certificates", "curl", "gnupg", "lsb-release"]
 
@@ -8,7 +8,7 @@ locals {
     "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
     "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
     "sudo apt-get update",
-    "sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin",
+    "sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin",
     "sudo usermod -aG docker ubuntu"
   ]
 
