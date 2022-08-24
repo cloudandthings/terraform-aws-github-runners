60df5ad75dc69a44bb4339ef3c97b4b8aab76a72
Bjorn Olsen <bjorn@cloudandthings.io>
2022-08-24 10:05:54 +0000

    f

--- modules/software/main.tf
+++ modules/software/main.tf
@@ -7,7 +7,7 @@ locals {
 
   runcmds = {
     "docker-engine" = [
-      "echo **** DOCKER ENGINE ****",
+      "echo ==== DOCKER-ENGINE ====",
       "sudo mkdir -p /etc/apt/keyrings",
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
       "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
@@ -17,7 +17,7 @@ locals {
     ]
 
     "terraform" = [
-      "echo **** TERRAFORM ****",
+      "echo ==== TERRAFORM ====",
       "wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg",
       "echo \"deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main\" | sudo tee /etc/apt/sources.list.d/hashicorp.list",
       "sudo apt-get update",
@@ -25,10 +25,12 @@ locals {
     ]
 
     "tflint" = [
+      "echo ==== TFLINT ====",
       "sudo curl -s https://raw.githubusercontent.com/terraform-linters/tflint/master/install_linux.sh | bash"
     ]
 
     "terraform-docs" = [
+      "echo ==== TERRAFORM-DOCS ====",
       "curl -sSLo ./terraform-docs.tar.gz https://terraform-docs.io/dl/v0.16.0/terraform-docs-v0.16.0-$(uname)-amd64.tar.gz",
       "tar -xzf terraform-docs.tar.gz",
       "chmod +x terraform-docs",
