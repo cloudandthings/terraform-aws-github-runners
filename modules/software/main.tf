be7f7f3fc9f7ed032c6a004abf1bbb267d27c96c
Bjorn Olsen <bjorn@cloudandthings.io>
2022-09-01 09:43:33 +0000

    f

--- modules/software/main.tf
+++ modules/software/main.tf
@@ -15,7 +15,7 @@ locals {
     "node"          = ["nodejs"]
     "pre-commit"    = ["pre-commit"]
     "python2"       = ["python2"]
-    "python3"       = ["python3", "python3-pip", "python3-venv"]
+    "python3"       = ["python3", "python3-pip", "python3-venv", "python-is-python3"]
     "tflint"        = ["unzip"]
   }
 
