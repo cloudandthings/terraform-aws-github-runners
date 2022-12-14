e74b64a983a0dca8455c2752fe0d7ffe949dbfcb
Bjorn Olsen <bjorn@cloudandthings.io>
2022-12-14 10:22:02 +0200

    fix(python3): Add python-dev-is-python3 package (#34)

--- modules/software/main.tf
+++ modules/software/main.tf
@@ -5,7 +5,7 @@ locals {
     "node"          = ["nodejs"]
     "pre-commit"    = ["pre-commit"]
     "python2"       = ["python2"]
-    "python3"       = ["python3", "python3-pip", "python3-venv", "python-is-python3"]
+    "python3"       = ["python3", "python3-pip", "python3-venv", "python-is-python3", "python-dev-is-python3"]
     "tflint"        = ["unzip"]
   }
 
