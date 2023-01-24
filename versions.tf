9fb1d466ab0938a3f6d232dd167013855a94e644
Bjorn Olsen <bjorn@cloudandthings.io>
2023-01-24 10:19:28 +0200

    chore: Fixup workflows

--- versions.tf
+++ versions.tf
@@ -1,18 +1,18 @@
 terraform {
-  required_version = ">= 0.13.1"
+  required_version = ">= 0.14.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.9, <5"
+      version = "~> 4.9"
     }
     http = {
       source  = "hashicorp/http"
-      version = "3.0.1"
+      version = "~> 3.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = "3.2.1"
+      version = "~> 3.2"
     }
   }
 }
