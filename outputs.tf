06836a980ec501fc56faadf4ce13577251b94fec
Andrew Cheesman <andrew@cloudandthings.io>
2024-12-05 15:17:31 +0200

    update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,24 @@
-output "codebuild_project" {
-  value = {
-    name = aws_codebuild_project.this.name
-    arn  = aws_codebuild_project.this.arn
-  }
-  description = "Name and ARN of codebuild project, to be used when running GitHub Actions"
+output "codebuild_project_name" {
+  value       = aws_codebuild_project.this.name
+  description = "Name of the codebuild project, to be used when running GitHub Actions"
 }
 
-output "codebuild_role" {
-  value = {
-    name = try(aws_iam_role.this[0].name, null)
-    arn  = try(aws_iam_role.this[0].arn, null)
-  }
-  description = "Name and ARN of codebuild role, to be used when running GitHub Actions"
+output "codebuild_project_arn" {
+  value       = aws_codebuild_project.this.arn
+  description = "ARN of the codebuild project, to be used when running GitHub Actions"
 }
 
-output "ecr_repository" {
-  value = {
-    name = try(aws_ecr_repository.this[0].name, null)
-    arn  = try(aws_ecr_repository.this[0].arn, null)
-  }
-  description = "Name and ARN of ECR repository, to be used when to push custom docker images for the codebuiild project"
+output "codebuild_role_name" {
+  value       = try(aws_iam_role.this[0].name, var.iam_role_name)
+  description = "Name of the codebuild role, to be used when running GitHub Actions"
+}
+
+output "ecr_repository_name" {
+  value       = try(aws_ecr_repository.this[0].name, null)
+  description = "Name of the ECR repository, to be used when to push custom docker images for the codebuiild project"
+}
+
+output "aws_security_group_id" {
+  value       = try(aws_security_group.codebuild[0].id, null)
+  description = "ID of the security group created for the codebuild project"
 }
