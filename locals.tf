34680cc12c26c0a7609f73b7043117cbc32c435e
Bob Sutterfield <bobsut@users.noreply.github.com>
2025-09-30 15:41:05 -0700

    add example README

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   aws_account_id = data.aws_caller_identity.current.account_id
 
-  aws_region = data.aws_region.current.name
+  aws_region = data.aws_region.current.region
 
   has_s3_log_bucket = var.s3_logs_bucket_name != null
 
